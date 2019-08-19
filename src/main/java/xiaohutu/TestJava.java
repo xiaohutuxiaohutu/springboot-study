eb12c93749e98b3e8926d06fc96183c8e3e33c11
xiaohutu <1328669873@qq.com>
2019-08-19 16:15:14 +0800

    test class

---
+++ src/main/java/xiaohutu/TestJava.java
@@ -0,0 +1,29 @@
+package xiaohutu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestJava {
+    public static void main(String[] args) {
+        String[] s1 = new String[0];
+        System.out.println(s1.length);
+        String[] strings2 = {"a"};
+        System.out.println(strings2.length);
+
+        /*List<String> list1 = null;
+        String[] strings4 = list1.toArray(new String[0]);
+        System.out.println("strings4:" + strings4.length);*/
+
+        List<String> list = new ArrayList<>();
+        System.out.println("list:" + list.size());
+        String[] strings3 = list.toArray(new String[0]);
+        System.out.println(strings3.length);
+        list.add("a");
+        list.add("b");
+        String[] strings = list.toArray(new String[0]);
+        System.out.println(strings.length);
+
+        String[] strings1 = list.toArray(new String[list.size()]);
+        System.out.println(strings1.length);
+    }
+}
